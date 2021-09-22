11c54fd53136c7be3975642130b0f313396a00a7
Diogo Franco <diogofrancoweb@gmail.com>
2021-09-22 11:48:57 -0300

    logo navbar mobile e desktop

--- components/navbar.js
+++ components/navbar.js
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export function NavTop(){
     return(
-      <Navbar className={StyleNav.navTop}>
+      <Navbar  className={StyleNav.navTop}>
         <Container>
             <Col xs md={{ span: 4, offset: 8 }} className="text-end">
             <FontAwesomeIcon icon={faLock}  />
@@ -28,37 +28,14 @@ export function NavTop(){
 
 export function NavLogo(){
   return(
-    <Navbar bg="light" expand="lg" style={{ height: '120px' }}>
+    <Navbar bg="light" className="d-none d-sm-block" expand="lg">
         <Container>
           <Col md={4}>
             <Image src="/logo-pmac.svg" alt="Vercel Logo" className="mt-2" width={222} height={130} />
           </Col>
           <Col md={{ span: 4, offset: 4 }} className="mr-2 text-end">
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="mr-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
-              navbarScroll
-            >
-            </Nav>
-            <Form className="d-flex ">
-              <FormControl
-                type="search"
-                placeholder="Busca"
-                className="mr-2 text-start"
-                aria-label="Search"
-              />
-            </Form>
-          </Navbar.Collapse>
-          </Col>
-          <Button className="nav-toggle">
-              <span className="bar-top"></span>
-              <span className="bar-mid"></span>
-              <span className="bar-bot"></span>
-          </Button>
-              
-                    
-          <Navbar.Toggle aria-controls="navbarScroll" />
+          
+          </Col> 
           
         </Container>
       </Navbar>
@@ -69,25 +46,43 @@ export function NavLogo(){
 
 export function NavMenu(){
   return(
-      
-    <Navbar bg="light">
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#" className="d-lg-none">
+          <Image src="/logo-pmac.svg" alt="Vercel Logo" className="mt-2" width={102} height={55} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="mr-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+        <Nav.Link href="#action1">Home</Nav.Link>
+        <Nav.Link href="#action2">Link</Nav.Link>
+        <NavDropdown title="Link" id="navbarScrollingDropdown">
+          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link href="#" disabled>
+          Link
+        </Nav.Link>
+      </Nav>
+      <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Search"
+          className="mr-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+    
   )
   
 }
